d224b7a114f3830d3f9cd7ccf5f0fc215d30695b
watasuke102 <watasuke102@gmail.com>
2020-07-16 06:51:14 +0000

    [update] : URL追加

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+
+	<h1>Welcome to my secondary homepage!!</h1>
+	<hr>
+	<h2>Links</h2>
+	<ul>
+		<li> <a href="http://menster.wp.xdomain.jp">    Main Homepage (blog and profile etc.) </a> </li>
+		<li> <a href="https://github.com/watasuke102">  GitHub                                </a> </li>
+		<li> <a href="https://soundcloud.com/watasuke"> SoundCloud                            </a> </li>
+	<ul>
+
+<body>
+</html>
