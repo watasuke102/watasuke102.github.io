01f1b36ccb71032f0ea6f5ee7bf776ee3c57dfc9
watasuke102 <watasuke102@gmail.com>
2020-07-16 13:15:08 +0000

    [update] : ヘッダーなどの内容追加

--- index.php
+++ index.php
@@ -6,10 +6,16 @@
 	<link rel="stylesheet" href="main.css">
 </head>
 
-<html>
+<html lang="ja-JP">
 
 <header>
-	<p> |-- here is header^^ --| </p>
+	<div class="header-left">
+		<p> |-- here is header --| </p>
+	</div>
+	<div class="header-right">
+		<a href="http://menster.wp.xdomain.jp">        メインのホームページ   </a>
+		<a href="http://menster.wp.xdomain.jp/profile">更に詳しいプロフィール </a>
+	</div>
 </header>
 
 <body>
@@ -23,8 +29,9 @@
 		<h3>わたすけ (<a href="https://twitter.com/Watasuke102"> @Watasuke102 </a>)</h3>
 		<p>
 			日本のょゎょゎ高専生<br>
+			<a href="https://fascode.net">FascodeNetwork</a>に所属しております<br>
 			東方、プログラミング、DTMなどをしてます<br>
-			技術: C++/PHP-HTML-CSS/Aviutl/3DCG
+			技術・触ったことあるもの: C++ / Python / PHP-HTML-CSS / Aviutl / 3DCG (Blender)
 		</p>
 	</div>
 
@@ -32,9 +39,10 @@
 	<div class="content">
 		<h2>リンク集</h2>
 		<ul>
-			<li> <a href="http://menster.wp.xdomain.jp">    Main Homepage (ブログとか)            </a> </li>
-			<li> <a href="https://github.com/watasuke102">  GitHub                                </a> </li>
-			<li> <a href="https://soundcloud.com/watasuke"> SoundCloud                            </a> </li>
+			<li> <a href="http://menster.wp.xdomain.jp">    Main Homepage (ブログとか) </a> </li>
+			<li> <a href="https://twitter.com/Watasuke102"> Twitter                    </a> </li>
+			<li> <a href="https://github.com/watasuke102">  GitHub                     </a> </li>
+			<li> <a href="https://soundcloud.com/watasuke"> SoundCloud                 </a> </li>
 		<ul>
 	</div>
 
