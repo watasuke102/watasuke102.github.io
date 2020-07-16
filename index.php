18f7c5ca3dc25307153c4a5a4f74447a17a61e21
watasuke102 <watasuke102@gmail.com>
2020-07-16 08:18:40 +0000

    [update] : ヘッダー、文章の追加

--- index.php
+++ index.php
@@ -1,10 +1,27 @@
 <!DOCTYPE html>
 
+<head>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap">
+	<link rel="stylesheet" href="main.css">
+</head>
+
 <html>
+
+<header>
+	<p> |-- here is header^^ --| </p>
+</header>
+
 <body>
 
 	<h1>Welcome to my secondary homepage!!</h1>
 	<hr>
+
+	<h2>’N</h2>
+	<p>
+		“ú–{‚Ì‚å‚ì‚å‚ì‚ê¶<br>
+		C++/PHP-HTML-CSS/Aviutl/3DCG
+	</p>
+
 	<h2>Links</h2>
 	<ul>
 		<li> <a href="http://menster.wp.xdomain.jp">    Main Homepage (blog and profile etc.) </a> </li>
