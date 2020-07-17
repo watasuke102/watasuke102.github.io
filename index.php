43a8527930493f564df38d1a2c7c872fa0cf8704
watasuke102 <watasuke102@gmail.com>
2020-07-17 03:17:41 +0000

    [update] : メタタグの追加 Twitterカード機能しない

--- index.php
+++ index.php
@@ -2,8 +2,37 @@
 
 <head>
 	<meta charset="utf-8">
+
+
+	<meta property="og:url"          content="http://watasuke.tk">
+	<meta property="og:title"        content="Welcome!">
+	<meta property="og:description"  content="わたすけ(@Watasuke102)のホームページです">
+	<meta property="og:image"        content="http://watasuke.tk/pic/icon.jpg">
+
+	<meta name="twitter:card"        content="summary">
+	<meta name="twitter:site"        content="@Watasuke102">
+	<meta name="twitter:title"       content="Welcome!">
+	<meta name="twitter:url"         content="http://watasuke.tk">
+	<meta name="twitter:image"       content="http://watasuke.tk/pic/icon.jpg">
+	<meta name="twitter:description" content="わたすけ(@Watasuke102)のホームページです">
+
+
+	<title>Welcome!</title>
+
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap">
 	<link rel="stylesheet" href="main.css">
+	<link rel="shortcut icon" href="pic/icon.jpg" type="image/jpg">
+
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-118931206-3"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'UA-118931206-3');
+	</script>
+
 </head>
 
 <html>
